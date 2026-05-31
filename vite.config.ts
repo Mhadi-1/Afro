42dbea459889f8a40d84d8120f7f0dec6ac91830
mhadi <mhody255@gmail.com>
2026-05-31 20:54:26 +0300

    Update homepage and base URL to match repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: "/Afro/", // 👈 ADD THIS LINE (Make sure to include the slashes)
+  plugins: [react()],
+  // ... rest of your config
 });
